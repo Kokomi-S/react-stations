b6ce1e867a91354cf9fe8c7d269147afa7a85a89
suzuki <kokomidx@gmail.com>
2025-09-06 20:26:04 +0900

    問03 解答

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ export const App = () => {
       <header>
         <h1>My App</h1>
       </header>
+      <img src='https://images.dog.ceo/breeds/frise-bichon/5.jpg' alt="犬の画像" />
+      <p>犬の画像を表示するサイトです</p>
     </div>
   )
 }

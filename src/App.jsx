1a1ed3b319da65a65ab3fad75f343c6b2fa245aa
suzuki <kokomidx@gmail.com>
2025-09-12 01:58:24 +0900

    問06 解答

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 // DO NOT DELETE
-
 import './App.css'
 import { useState } from 'react';
 
@@ -7,15 +6,34 @@ import { useState } from 'react';
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  
   const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/frise-bichon/5.jpg');
+  const [isLoading, setIsLoading] = useState(false);
+
+
+  const fetchNewDog = async() => {
+    try {
+      setIsLoading(true);
+      const res = await fetch('https://dog.ceo/api/breeds/image/random');
+      if (!res.ok) throw new Error('ネットワークエラー');
+      const data = await res.json();
+      setDogUrl(data.message);
+    } catch (error) {
+      console.error('エラーが発生しました', error);
+    } finally {
+      setIsLoading(false); 
+    }
+  };
+
   return (
     <div>
       <header>
         <h1>My App</h1>
       </header>
+      {isLoading && <p>読み込み中...</p>}
       <img src= {dogUrl} alt= "犬の画像" />
       <p>犬の画像を表示するサイトです</p>
-      <button onClick={() => setDogUrl('https://images.dog.ceo/breeds/frise-bichon/2.jpg')}>更新</button>
+      <button onClick={fetchNewDog}>更新</button>
     </div>
   )
 }

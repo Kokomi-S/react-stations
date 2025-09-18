216f43cb64eb0c056547f7789231fad087958abd
suzuki <kokomidx@gmail.com>
2025-09-18 16:29:31 +0900

    問09 解答

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Header from './Header'
 import Description from './Description'
+import DogListContainer from './DogListContainer'
 
 
 /**
@@ -12,6 +13,7 @@ export const App = () => {
     <>
       <Header />
       <Description />
+      <DogListContainer />
     </>
   );
 
--- src/DogListContainer.jsx
+++ src/DogListContainer.jsx
@@ -1,7 +1,35 @@
 // @ts-check
+import { useState, useEffect } from 'react';
 
 export const DogListContainer = () => {
-  return <></>
+  const [breeds , setBreeds] = useState([]);
+  const fetchBreeds = async() => {
+    try {
+      const res = await fetch('https://dog.ceo/api/breeds/list/all');
+      if (!res.ok) throw new Error('ネットワークエラー');
+      const data = await res.json();
+      setBreeds(Object.keys(data.message));
+    } catch (error) {
+
+
+      console.error('エラーが発生しました', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchBreeds();
+    return () => {};
+  },[]);
+
+  return (
+    <select>
+      {breeds.map((breed) => (
+        <option key={breed} value={breed}>
+        {breed}
+        </option>
+      ))}
+    </select>
+  );
 }
 
 export default DogListContainer
